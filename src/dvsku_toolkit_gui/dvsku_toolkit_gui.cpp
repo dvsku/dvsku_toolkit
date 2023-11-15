5e84b7c7c6e572605ffe8a04efb8bc34d691fdfa
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-15 16:37:36 +0100

    Remove old project

--- src/dvsku_toolkit_gui/dvsku_toolkit_gui.cpp
+++
@@ -1,6 +0,0 @@
-#include "gui.h"
-
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
-    dvsku::toolkit::gui::instance().start(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
-    return 0;
-}
