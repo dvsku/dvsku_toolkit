2fb7d4d7164dfd88b23ccf802df26ec636eb88ab
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-21 04:44:52 +0100

    Add evp system to systems bundle

--- src/dvsku_toolkit/systems/systems_bundle.cpp
+++ src/dvsku_toolkit/systems/systems_bundle.cpp
@@ -1,3 +1,6 @@
 #include "systems/systems_bundle.hpp"
 
 using namespace dvsku_toolkit;
+
+systems_bundle::systems_bundle() 
+    : evp(*this) {}
--- src/dvsku_toolkit/systems/systems_bundle.hpp
+++ src/dvsku_toolkit/systems/systems_bundle.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "systems/sys_evp.hpp"
+
 namespace dvsku_toolkit {
     class systems_bundle {
-    
+    public:
+        sys_evp evp;
+
+    public:
+        systems_bundle();
     };
 }
