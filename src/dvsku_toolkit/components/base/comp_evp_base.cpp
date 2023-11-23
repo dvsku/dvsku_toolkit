ec5bd3a22daf8d8ff5090e3906af7a700ef364f2
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-23 06:47:41 +0100

    Update taskbar on handlers

--- src/dvsku_toolkit/components/base/comp_evp_base.cpp
+++ src/dvsku_toolkit/components/base/comp_evp_base.cpp
@@ -1,5 +1,6 @@
 #include "components/base/comp_evp_base.hpp"
 #include "components/components_bundle.hpp"
+#include "systems/systems_bundle.hpp"
 
 using namespace dvsku_toolkit;
 
@@ -9,9 +10,9 @@ comp_evp_base::comp_evp_base(components_bundle& components)
 void comp_evp_base::handle_on_start() {
     m_components.view.is_working = true;
 
-    //GUI.set_disabled(true);
-    //GUI.set_taskbar_status(TBPF_NORMAL);
-    //GUI.set_taskbar_progress(0.0f);
+    m_components.systems.taskbar.set_status(TBPF_NORMAL);
+    m_components.systems.taskbar.set_progress(0.0f);
+
     m_progress = 0.0f;
     sprintf(m_progress_text, "%.2f%c", m_progress, '%');
 }
@@ -34,20 +35,24 @@ void comp_evp_base::handle_on_finish(bool success) {
     m_cancel                     = false;
     m_components.view.is_working = false;
 
-    //GUI.set_disabled(false);
-    //GUI.set_taskbar_status(TBPF_NOPROGRESS);
-    //GUI.set_taskbar_progress(0.0f);
+    m_components.systems.taskbar.set_status(TBPF_NOPROGRESS);
+    m_components.systems.taskbar.set_progress(0.0f);
+
     //PlaySound((LPCTSTR)SND_ALIAS_SYSTEMASTERISK, NULL, SND_ALIAS_ID | SND_SYSTEM | SND_ASYNC);
 }
 
 void comp_evp_base::handle_on_update(float progress) {
     m_progress += progress;
     sprintf(m_progress_text, "%.2f%c", m_progress, '%');
-    /*GUI.set_taskbar_progress(m_progress);*/
+
+    m_components.systems.taskbar.set_progress(m_progress);
 }
 
 void comp_evp_base::handle_on_error(const std::string& msg) {
-    /*GUI.set_disabled(false);
-    GUI.set_taskbar_status(TBPF_ERROR);
-    PlaySound((LPCTSTR)SND_ALIAS_SYSTEMHAND, NULL, SND_ALIAS_ID | SND_SYSTEM | SND_ASYNC);*/
+    m_components.view.is_working = false;
+
+    m_components.systems.taskbar.set_status(TBPF_ERROR);
+    m_components.systems.taskbar.set_progress(0.0f);
+
+    //PlaySound((LPCTSTR)SND_ALIAS_SYSTEMHAND, NULL, SND_ALIAS_ID | SND_SYSTEM | SND_ASYNC);
 }
