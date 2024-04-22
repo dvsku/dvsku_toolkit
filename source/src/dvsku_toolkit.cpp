2f03d6f5d9dde9c70ed11312b3b7670ab8c0ac92
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-04-22 18:30:23 +0200

    Update window size

--- source/src/dvsku_toolkit.cpp
+++ source/src/dvsku_toolkit.cpp
@@ -7,7 +7,7 @@ using namespace dvsku_toolkit;
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
     try {
-        dt_app app(650, 400, DT_APP_NAME);
+        dt_app app(600, 275, DT_APP_NAME);
         app.run();
     }
     catch (...) {
