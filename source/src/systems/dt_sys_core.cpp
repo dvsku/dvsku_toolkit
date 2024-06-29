126151d78a07a9b22b26a4ef7a1cd1d6c0de9fe7
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-06-29 16:32:32 +0200

    Add work context

--- source/src/systems/dt_sys_core.cpp
+++ source/src/systems/dt_sys_core.cpp
@@ -1,4 +1,5 @@
 #include "systems/dt_sys_core.hpp"
+#include "dt_app.hpp"
 #include "dvsku_toolkit.hpp"
 
 using namespace dvsku_toolkit;
@@ -7,7 +8,8 @@ using namespace dvsku_toolkit;
 // PUBLIC
 
 dt_sys_core::dt_sys_core(dt_app& app)
-    : dt_system_base(app)
+    : dt_system_base(app),
+      work_context(static_cast<libgui::window&>(app))
 {
     app_version = libgui::version(DT_VERSION_MAJOR, DT_VERSION_MINOR, DT_VERSION_REV);
 };
--- source/src/systems/dt_sys_core.hpp
+++ source/src/systems/dt_sys_core.hpp
@@ -3,6 +3,7 @@
 #include "systems/dt_system_base.hpp"
 
 #include <libgui.hpp>
+#include <libgui/misc/work_context.hpp>
 #include <mutex>
 
 namespace dvsku_toolkit {
@@ -10,21 +11,12 @@ namespace dvsku_toolkit {
 
     class dt_sys_core : public dt_system_base {
     public:
-        libgui::version app_version;
-        bool            is_working = false;
-        bool            has_errors = false;
-        std::string     errors     = "";
-
-        std::mutex mutex;
+        libgui::version      app_version;
+        libgui::work_context work_context;
+        std::string          errors = "";
+        std::mutex           mutex;
 
     public:
-        dt_sys_core()                   = delete;
-        dt_sys_core(const dt_sys_core&) = delete;
-        dt_sys_core(dt_sys_core&&)      = delete;
-
-        dt_sys_core& operator=(const dt_sys_core&) = delete;
-        dt_sys_core& operator=(dt_sys_core&&)      = delete;
-
         dt_sys_core(dt_app& app);
     };
 }
